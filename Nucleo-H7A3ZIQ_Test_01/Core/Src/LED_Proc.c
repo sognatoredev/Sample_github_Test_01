f5d108e7f34ec447448259c2f1297b5fa89c6fdb
sognatore <off.sognatore@gmail.com>
2024-08-05 14:58:55 +0900

    Test 03

--- Nucleo-H7A3ZIQ_Test_01/Core/Src/LED_Proc.c
+++ Nucleo-H7A3ZIQ_Test_01/Core/Src/LED_Proc.c
@@ -83,3 +83,13 @@ void LED_Process (void)
     HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
 }
 */
+
+/*
+void LED_Process (void)
+{
+    HAL_Delay(500);
+    HAL_GPIO_TogglePin(GPIOB, LD1_Pin);
+    HAL_GPIO_TogglePin(GPIOB, LD3_Pin);
+    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+}
+*/
