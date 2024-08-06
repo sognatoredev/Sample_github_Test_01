a0aa3a5e48cca8ff5d7b0a1cf876f8f3218ebdd1
sognatore <off.sognatore@gmail.com>
2024-08-06 13:23:23 +0900

    GPIO 입력 상태 판단. 스위치 토글 판단하여 출력. 인터럽트 X

--- Nucleo-H7A3ZIQ_Test_01/Core/Src/button_proc.c
+++ Nucleo-H7A3ZIQ_Test_01/Core/Src/button_proc.c
@@ -13,21 +13,22 @@
 /* Includes ------------------------------------------------------------------*/
 #include "button_proc.h"
 
+uint8_t prvbtnstate = 0;
 
 #if 0
-void LED_Process (void)
-{
-    HAL_Delay(500);
-    HAL_GPIO_TogglePin(GPIOB, LD1_Pin);
-    HAL_GPIO_TogglePin(GPIOB, LD3_Pin);
-    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-}
+
 #else
 void Button_Process (void)
 {
-    if (GPIO_PIN_SET == HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin))
+    uint8_t currentlybtnstate = 0;
+    
+    currentlybtnstate = HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin);
+
+    if (prvbtnstate != currentlybtnstate)
     {
-        sprintf(uart3_tx_buf, "Button State : Pushed.\r\n");
+        prvbtnstate = currentlybtnstate;
+
+        sprintf(uart3_tx_buf, "Button Toggle.\r\n");
         HAL_UART_Transmit(&huart3, (uint8_t *) uart3_tx_buf, strlen(uart3_tx_buf), HAL_MAX_DELAY);
     }
 }
