6a019d657c34a9331f85bdb7f37e78961ab12333
sognatore <off.sognatore@gmail.com>
2024-08-14 09:26:13 +0900

    UART3 System Reset cmd  추가

--- Nucleo-H7A3ZIQ_Test_01/Core/Src/uart_proc.c
+++ Nucleo-H7A3ZIQ_Test_01/Core/Src/uart_proc.c
@@ -28,6 +28,12 @@ void Uart3_Process (void)
             sprintf(uart3_tx_buf, "In B\r\n");
             HAL_UART_Transmit(&huart3, (uint8_t *) uart3_tx_buf, strlen(uart3_tx_buf), HAL_MAX_DELAY);
         }
+        else if (!strncmp("SYSTEM RESET", uart3_rx_buf, UART3_CMP_SIZE))
+        {
+            sprintf(uart3_tx_buf, "STM32H7A3 System Reset.\r\n");
+            HAL_UART_Transmit(&huart3, (uint8_t *) uart3_tx_buf, strlen(uart3_tx_buf), HAL_MAX_DELAY);
+            HAL_NVIC_SystemReset();
+        }
 
         //memset(uart3_rx_buf, 0, uart3_rx_index);
         uart3_rx_index = 0;
